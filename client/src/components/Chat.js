56bb924f6db05ad766743917b5c61847ca3d77f7
Sai Swarup Rath <saiswaruprath7@gmail.com>
2023-05-02 23:39:56 -0700

    Create Chat.js

---
+++ client/src/components/Chat.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+        
+    export class Chat extends Component {
+      componentDidMount(){
+            (function(d, m){
+                var kommunicateSettings = 
+                    {"appId":"2ce1a0e563d25c5b7b1d45e4f3fbc1b58","popupWidget":true,"automaticChatOpenOnNavigation":true};
+                var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+                s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+                var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+                window.kommunicate = m; m._globals = kommunicateSettings;
+            })(document, window.kommunicate || {});
+        }
+      render() {
+        return(
+            <div></div>
+
+        )
+      }      
+        
+    }
+  
+/* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
+
+    
+
+
+export default Chat
